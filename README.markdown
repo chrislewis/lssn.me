791c7a8f86f1bee126fa9b45771009976577e7cb
chrislewis <chris@thegodcode.net>
2010-12-28 07:42:23 -0500

    placeholder for non html5; readme; shortened links only render once

---
+++ README.markdown
@@ -0,0 +1,12 @@
+[lssn.me][lm] is a minimal URL shortening application. In addtion to a functioning URL shortener,
+[lssn.me][lm] is intended as a sort reference application for deploying simple [scala][scala] applications to
+[Google App Engine](http://appengine.google.com/)
+
+## Reference For ... ?
+Okay, so part of the agenda is to show off [Unfiltered][uf] and [highchair][hc] together. [Unfiltered][uf] is a toolkit for servicing HTTP requests in scala,
+and [highchair][hc] is a library for presisting scala objects to the Google Datastore.
+
+[lm]: http://lssn.me/
+[scala]: http://www.scala-lang.org/
+[uf]: http://github.com/n8han/unfiltered
+[hc]: http://github.com/chrislewis/highchair
--- src/main/webapp/js/lssn.js
+++ src/main/webapp/js/lssn.js
@@ -1,13 +1,19 @@
 (function($) {
   $(document).ready(function() {
+    $('input').placeholder();
+    
+    /* TODO validation + error handling */
     $('#lssn').submit(function(e) {
       e.preventDefault();
       $.post(this.action, $(this).serialize(), function(e) {
-        var url = location.protocol + '//' + location.host + '/' + e.shortUrl;
-        var link = $('<li>').append(
-          $('<a>').attr({ href: url }).text(url)
-        ).hide();
-        $('#shortened').prepend(link.fadeIn());
+        var id = 'url-' + e.shortUrl;
+        if($('#' + id).length < 1) {
+          var url = 'http://lssn.me/' + e.shortUrl;
+          var link = $('<li>').append(
+            $('<a>').attr({ id: id, href: url }).text(url)
+          ).hide();
+          $('#shortened').prepend(link.fadeIn());
+        }
       });
     })
   });
